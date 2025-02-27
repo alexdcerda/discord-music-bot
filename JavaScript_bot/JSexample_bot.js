80d97aba3f7f22932fd9f231f4fbe386e368e929
Alex Cerda <alexdcerda@outlook.com>
2025-02-26 21:46:12 -0500

    example js bot

--- JavaScript_bot/JSexample_bot.js
+++ JavaScript_bot/JSexample_bot.js
@@ -1,33 +1,43 @@
-import { Client, GatewayIntentBits } from "discord.js";
-import fs from "fs";
-import path from "path";
-import dotenv from "dotenv";
-import ytdl from "ytdl-core";
-import url from "url";
+import dotenv from 'dotenv'
+import { Client, GatewayIntentBits } from 'discord.js'
+import ytdl from 'ytdl-core'
+import { joinVoiceChannel, createAudioPlayer, createAudioResource } from '@discordjs/voice';
+import { ytSearch } from 'yt-search'
 
 
-
-require("dotenv").config();
-
-const { Client, GatewayIntentBits } = require("discord.js");
-
-const TOKEN = process.env.DISCORD_TOKEN;
-
+dotenv.config({path: '../.env'})
+const TOKEN = process.env.DISCORD_TOKEN
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.GuildVoiceStates,
   ]
 })
 
 
-client.once('bot resady ---- im alive sir', () => {
-  console.log(`Logged in as ${client.user.tag}`)
+const prefix = '!'
+const server = {}
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} is now jamming`)
 })
 
 
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return
+  if (!message.content.startsWith(prefix)) return
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/)
+  const command = args.shift().toLowerCase()
+
+  if (command === 'play') {
+
+  }
+})
+
+
+
+
 client.login(TOKEN)
-
-
