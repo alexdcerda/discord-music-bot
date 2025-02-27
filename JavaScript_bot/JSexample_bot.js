c323a32eba0bb22dedbf5684760075ca9d574d33
Alex Cerda <alexdcerda@outlook.com>
2025-02-26 20:42:06 -0500

    updating JS bot

--- JavaScript_bot/JSexample_bot.js
+++ JavaScript_bot/JSexample_bot.js
@@ -1,9 +1,33 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+import ytdl from "ytdl-core";
+import url from "url";
 
 
 
+require("dotenv").config();
 
-function run_bot() {
-  return {
-    
-  };
-}
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+
+client.once('bot resady ---- im alive sir', () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
+
+client.login(TOKEN)
+
+
